1a2507d7e887823f9260ea16e43b4ccd4ee5b41d
Geoff Reid <geoffrey.reid@dell.com>
2017-06-12 14:30:49 -0400

    add publish script

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+#if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing to gh-pages...\n"
+
+  git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages
+
+  cd gh-pages
+  pwd
+  git rm -rf *
+  cp -Rf ../static/* .
+  git config user.name ${GIT_USER_NAME} 
+  git config user.email ${GIT_USER_EMAIL} 
+  git commit -m "Test"
+  git push -fq origin gh-pages
+
+#fi 
